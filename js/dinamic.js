dd76d313db86ac4f65c71e6705e6d38ebe84f768
JairoRC05 <64911345+JairoRC05@users.noreply.github.com>
2020-12-29 18:02:35 -0600

    Main

---
+++ js/dinamic.js
@@ -0,0 +1,9 @@
+
+        $("#menu-toggle").click(function (e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });
+
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
---
+++ js/divs/reclutamiento.js
@@ -0,0 +1,87 @@
+        $(document).ready(function () {
+            /*General*/
+            $('.head').load('includes/head.html')
+            $('.m-responsivo').load('includes/m-responsivo.html')
+            $('.n-navbar').load('includes/navbar.html')
+            $('.s-sidebar').load('includes/sidebar.html')
+            $('.navbar-can').load('includes/navbar-can.html')
+            $('.sidebar-can').load('includes/sidebar-can.html')
+            /*Dashboard*/
+            $('.title-dashboard').load('includes/Titles/title-dashboard.html')
+            $('.table-dashboard').load('includes/Tablas/Dashboard/tabla-dashboard.html')
+            /*Candidatos */
+            $('.title-candidato').load('includes/Titles/title-candidato.html')
+            $('.tabs-candidatos').load('includes/Tabs/tabs-candidatos.html')
+            $('.filtro-candidatos').load('includes/Filtros/filtro-req-altern-can.html')
+            $('.tabla-consultar').load('includes/Tablas/Candidatos/tabla-consultar.html')
+            $('.tabla-pasados').load('includes/Tablas/Candidatos/tabla-pasados.html')
+            $('.tabla-consultar').load('includes/Tablas/Candidatos/tabla-consultar.html')
+            /*Editar Candidato*/
+            $('.title-edit-candidato').load('includes/Titles/title-edit-candidato.html')
+            $('.tab-editar').load('includes/Tabs/tab-editar.html')
+            $('.tab-datos-generales').load('includes/Tabs/tab-datos-generales.html')
+            $('.tab-pane-datos').load('includes/TabPane/datos-generales.html')
+            $('.tab-pane-info').load('includes/TabPane/informacion-general.html')
+            
+            /*Entrevistas*/
+            $('.filtro-entrevistas').load('includes/Filtros/filtro-entrevistas.html')
+            
+
+            /*Exámenes*/
+            $('.title-examenes').load('includes/Titles/title-examenes.html')            
+            $('.tabla-examenes').load('includes/Tablas/Examenes/tabla-examenes.html')
+            /*Plantillas*/
+            $('.title-plantillas').load('includes/Titles/title-plantillas.html')
+            $('.filtro-plantillas').load('includes/Filtros/filtro-plantillas.html')
+            $('.table-plantillas').load('includes/Tablas/Plantillas/table-plantillas.html')
+            /*Requerimientos*/
+            $('.title-requerimientos').load('includes/Titles/title-requerimientos.html')
+            $('.tabs-requerimientos').load('includes/Tabs/tabs-requerimientos.html')
+            $('.filtro-req').load('includes/Filtros/filtro-req.html')
+            $('.filtro-req-altern').load('includes/Filtros/filtro-req-altern.html')
+            $('.filtro-req-sus').load('includes/Filtros/filtro-req-sus.html')
+            $('.table-actuales').load('includes/Tablas/Requerimientos/table-actuales.html')
+            $('.table-suspension').load('includes/Tablas/Requerimientos/table-suspension.html')
+            $('.table-concluidos').load('includes/Tablas/Requerimientos/table-concluidos.html')     
+            /*Detalle Req*/
+            $('.tab-detalle').load('includes/Tabs/tab-detalle.html')
+            $('.title-detalle').load('includes/Titles/title-detalle.html')
+            $('.tabla-registrados').load('includes/Tablas/DetalleReq/tabla-registrados.html')
+            $('.tabla-registrado').load('includes/faf.html')
+            $('.tabla-proceso').load('includes/Tablas/DetalleReq/tabla-proceso.html')
+            $('.tabla-descartados').load('includes/Tablas/DetalleReq/tabla-descartados.html')
+            $('.tabla-finalistas').load('includes/Tablas/DetalleReq/tabla-finalistas.html')
+            $('.btn-candidato').load('includes/Buttons/btn-candidato.html')
+           
+           
+            /*Exámenes Candidato*/
+            $('.title-examen').load('includes/Titles/title-examen-candidato.html')
+            $('.tabla-examenes-can').load('includes/Tablas/ExamenesCandidatos/tabla-examenes.html')
+            
+
+
+
+            /*Modales*/
+            $('.cerrar-sesion').load('includes/Modales/m-cerrar-sesion.html')
+            $('.historial-entrevistas').load('includes/Modales/modal-historial-entrevistas.html')
+            $('.nuevo-candidato').load('includes/Modales/nuevo-candidato.html')
+            $('.cv').load('includes/Modales/cv.html')
+            $('.observaciones-candidatos').load('includes/Modales/observaciones-candidatos.html')
+            $('.baja').load('includes/Modales/baja.html')
+            $('.restaurar').load('includes/Modales/restaurar.html')
+            $('.preview').load('includes/Modales/preview.html')
+            $('.newReq').load('includes/Modales/newReq.html')
+            $('.reactivar-req').load('includes/Modales/reactivar-req.html')
+            $('.suspender-req').load('includes/Modales/suspender-req.html')
+            $('.baja-req').load('includes/Modales/baja-req.html')
+            $('.addEx').load('includes/Modales/agregar-examen.html')
+            $('.editEx').load('includes/Modales/editar-examen.html')
+            $('.detalles-examen').load('includes/Modales/detalles-examen.html')
+            $('.nueva-entrevista').load('includes/Modales/nueva-entrevista.html')
+            $('.editar-entrevista').load('includes/Modales/editar-entrevista.html')
+            $('.detalles-candidato').load('includes/Modales/detalles-candidato.html')
+        });
+
+
+
+    
